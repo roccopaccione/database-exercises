625ae78ac1e08f6d09ecb0448d955aa2764c4e03
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-26 16:00:27 -0500

    feat: finished albums file and albums sql table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,15 @@
+DROP DATABASE IF EXISTS codeup_test_db;
+CREATE DATABASE codeup_test_db;
 USE codeup_test_db;
+
 DROP TABLE IF EXISTS albums;
+
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(25),
-    name VARCHAR(45),
-    release_date DATE,
-    sales FLOAT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(75) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR(25),
     primary key (id)
 );

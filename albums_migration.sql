3f8cece749fed874e9d339259b50126032a043a6
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-26 15:34:16 -0500

    MySQL: created album migration script for tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums(
+CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(25),
     name VARCHAR(45),
-    release_date INT,
+    release_date DATE,
     sales FLOAT,
     genre VARCHAR(25),
     primary key (id)
